b80d282c53def7d9f25267ed64b4cd611ee45fe8
dvirsegev <dvirsegev@gmail.com>
2023-01-17 18:47:46 +0200

    Add S3 Bucket

---
+++ python/demo/app/api/endpoints/day2Operations/addS3Bucket.py
@@ -0,0 +1,26 @@
+import logging
+from fastapi import APIRouter
+
+from clients import port
+from schemas.webhook import Webhook
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+addS3BucketRouter = APIRouter()
+
+@addS3BucketRouter.post("/addS3Bucket")
+async def addS3Bucket(webhook: Webhook):
+    action_type = webhook.payload['action']['trigger']
+    action_identifier = webhook.payload['action']['identifier']
+    entity_identifier = webhook.payload['entity']['identifier']
+    properties = webhook.payload['properties']
+    blueprint = webhook.context.blueprint
+
+    if action_type == 'DAY-2' and action_identifier == 'addS3Bucket':
+        run_id = webhook.context.runId
+
+        message = 'Pr Opened successfully'
+        action_status = 'SUCCESS'
+        port.update_action(run_id, message, action_status)
+        return {'status': action_status}
--- python/demo/app/main.py
+++ python/demo/app/main.py
@@ -15,7 +15,7 @@ from api.endpoints.day2Operations.extendEnvironmentTTL import extendEnvironmentT
 from api.endpoints.day2Operations.changeOwnership import changeOwnershipRouter
 from api.endpoints.day2Operations.rollbackRunningService import rollbackRunningServiceRouter
 from api.endpoints.day2Operations.changeOnCall import changeOnCallRouter
-
+from api.endpoints.day2Operations.addS3Bucket import addS3BucketRouter
 from core.config import settings
 
 app = FastAPI(
@@ -36,6 +36,7 @@ app.include_router(extendEnvironmentTTLRouter, prefix=settings.API_STR)
 app.include_router(changeOwnershipRouter, prefix=settings.API_STR)
 app.include_router(rollbackRunningServiceRouter, prefix=settings.API_STR)
 app.include_router(changeOnCallRouter, prefix=settings.API_STR)
+app.include_router(addS3BucketRouter, prefix=settings.API_STR)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=3006, reload=True)
