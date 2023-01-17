fd63b391685db6c30c99b0e471699191430cc129
danielsinai <daniel.sinai.main@gmail.com>
2023-01-17 11:50:59 +0200

    removed settigns

--- python/demo/app/core/config.py
+++ python/demo/app/core/config.py
@@ -6,9 +6,6 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "demo"
     PORT_API_URL: str = "https://demo.getport.io:3000/v1"
     PORT_S3_BUCKET_BLUEPRINT: str = "S3Bucket"
-    PORT_CLIENT_ID: str = '60EsooJtOqimlekxrNh7nfr2iOgTcyLZ'
-    PORT_CLIENT_SECRET: str = 'Tho9hoyjQYomojauapUM3XCC5vMkVO5SWUAxWWGPAb4rQaPexG3jhQPB5WKUIoDx'
-
 
     class Config:
         case_sensitive = True
