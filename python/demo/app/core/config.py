04ad2cedd429fab20a97639954f49ae460240b18
dvirsegev <dvirsegev@gmail.com>
2023-03-22 18:42:30 +0200

    Update port api url

--- python/demo/app/core/config.py
+++ python/demo/app/core/config.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     API_STR: str = "/api"
     PROJECT_NAME: str = "demo"
-    PORT_API_URL: str = "https://demo.getport.io:3000/v1"
+    PORT_API_URL: str = "https://backend.demo.getport.io/v1"
     PORT_S3_BUCKET_BLUEPRINT: str = "S3Bucket"
 
     class Config:
